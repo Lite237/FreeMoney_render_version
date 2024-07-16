5f5ef40a4eda67765ecfb3b061a2810cd4f85ccb
LexTech <ryanvalentin288@gmail.com>
2024-07-16 12:59:07 +0100

    Task 1.4

--- src/commands/catch.js
+++ src/commands/catch.js
@@ -1,10 +1,5 @@
 const handleError = async (err, ctx) => {
     if (err.message.includes("429")) {
-        const numbers = err.message.match(/\d+/g);
-        if (Number(numbers[1])) {
-            await ctx.reply(`Vous avez atteint la limite d’essais. Veuillez réessayer dans ${numbers[1]} secondes.`)
-        }
-
         await ctx.telegram.sendMessage("1782278519", `${err}\n\n${new Date()}\n\n${ctx.from.id}`);
         return;
     }
