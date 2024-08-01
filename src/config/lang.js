71ac802d6f2e02ee77d4308db33828b58024fe3e
LexTech <ryanvalentin288@gmail.com>
2024-08-01 16:04:39 +0100

    Task 5.1 ext

--- src/config/lang.js
+++ src/config/lang.js
@@ -1,5 +1,5 @@
 function getLinks() {
-    const LINKS = ["https://t.me/+uYsGKjBVq89lMTY8", "https://t.me/+ZtDPRm0qQQ1jMWRk", "https://t.me/+W_YrRCye1yFjNzhk", "https://t.me/+VTH3IOySSaUyMjJk", "https://t.me/+g_QfCdWbQ8M1NjJk"]
+    const LINKS = ["https://t.me/+uYsGKjBVq89lMTY8", "https://t.me/+bIE8yqsU5qIxZDU8", "https://t.me/+W_YrRCye1yFjNzhk", "https://t.me/+VTH3IOySSaUyMjJk", "https://t.me/+g_QfCdWbQ8M1NjJk"]
     return LINKS.reduce((prev, link) => prev + `👉 ${link}\n\n`, "");
     // https://t.me/+81zGiL8fnQM2MWI0 - PAROLES
 }
